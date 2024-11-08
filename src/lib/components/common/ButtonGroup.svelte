e83d56516c50cd1428eebf4c3e04facaafff1e65
David Ding <david.ding@outlook.com>
2024-11-08 20:58:31 +1100

    add border shadow to button group

--- src/lib/components/common/ButtonGroup.svelte
+++ src/lib/components/common/ButtonGroup.svelte
@@ -21,11 +21,16 @@
   export { classNames as class };
 </script>
 
-<span class={mergeClassNames(["isolate inline-flex rounded-md w-full h-10", classNames])}>
+<span
+  class={mergeClassNames([
+    "isolate inline-flex rounded-md w-full h-10 border-b shadow",
+    classNames,
+  ])}
+>
   {#each items as item, index}
     <Button
       class={mergeClassNames([
-        "relative inline-flex items-center text-sm font-semibold border flex-1 justify-center",
+        "relative inline-flex items-center text-sm font-semibold border border-b-0 flex-1 justify-center",
         {
           "hover:bg-gray-600 bg-gray-700 text-white": item.isActive,
           "hover:bg-gray-100 bg-white": !item.isActive,
